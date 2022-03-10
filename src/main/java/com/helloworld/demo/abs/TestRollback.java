f995f55169f0e5ca3df83d0e39a5c09a691de393
刘永涛 <liuyt-g@glodon.com>
2022-03-10 18:43:07 +0800

    test rollback 2

--- src/main/java/com/helloworld/demo/abs/TestRollback.java
+++ src/main/java/com/helloworld/demo/abs/TestRollback.java
@@ -2,6 +2,7 @@ package com.helloworld.demo.abs;
 
 public class TestRollback {
     public static void main(String[] args) {
-        System.out.println("1111");
+        System.out.println("1111------>");
+        System.out.println("2222");
     }
 }
